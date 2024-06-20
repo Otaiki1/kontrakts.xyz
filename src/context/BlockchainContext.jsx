a029cfd169e096651d8f34d52153befd31248291
Otaiki1 <otaikisadiq2804@gmail.com>
2024-06-20 11:24:18 +0100

    refac: update context to allow for contract calls

--- src/context/BlockchainContext.jsx
+++ src/context/BlockchainContext.jsx
@@ -39,6 +39,159 @@ export const BlockchainContextProvider = ({ children }) => {
   };
 
 
+  // A function that takes a solidity output as a string and returns a readable time as a string
+  function convertToReadableTime(solidityOutput) {
+    // Convert the BigNumber object to a number
+    const seconds = Number(solidityOutput);
+
+    // Create a Date object from the number
+    const date = new Date(seconds * 1000); // multiply by 1000 to get milliseconds
+
+    // Format the Date object as a readable string
+    const readableTime = date.toLocaleString(); // use your preferred options
+
+    // Return the readable time
+    return readableTime;
+  }
+
+  const createAgreement = async (party2Address, tokenURI) => {
+    try {
+      // Connect to a custom JSON-RPC endpoint
+      const provider = new ethers.BrowserProvider(window.ethereum);
+      const signer = await provider.getSigner();
+
+      const contract = new ethers.Contract(CONTRACT_ADDRESS, abi, signer);
+
+      const tx = await contract.createAgreement(party2Address, tokenURI);
+      await tx.wait();
+      return true
+    } catch (err) {
+      console.log("error is ---",err);
+      return false;
+    }
+  };
+
+  const signAgreement = async (agreementId) => {
+    try {
+      // Connect to a custom JSON-RPC endpoint
+      const provider = new ethers.BrowserProvider(window.ethereum);
+      const signer = await provider.getSigner();
+
+      const contract = new ethers.Contract(CONTRACT_ADDRESS, abi, signer);
+
+      const tx = await contract.party2SignAgreement(agreementId);
+      await tx.wait();
+      return true;
+    } catch (err) {
+      console.log("error is ---",err);
+      return false;
+    }
+  };
+
+  const mintAgreement = async (agreementId) => {
+    try {
+      // Connect to a custom JSON-RPC endpoint
+      const provider = new ethers.BrowserProvider(window.ethereum);
+      const signer = await provider.getSigner();
+
+      const contract = new ethers.Contract(CONTRACT_ADDRESS, abi, signer);
+
+      const tx = await contract.mintNFTAgreement(agreementId);
+      await tx.wait();
+      return true;
+    } catch (err) {
+      console.log("error is ---",err);
+      return false;
+    }
+  };
+
+  const deleteAgreement = async (agreementId) => {
+    try {
+      // Connect to a custom JSON-RPC endpoint
+      const provider = new ethers.BrowserProvider(window.ethereum);
+      const signer = await provider.getSigner();
+
+      const contract = new ethers.Contract(CONTRACT_ADDRESS, abi, signer);
+
+      const tx = await contract.deleteContract(agreementId);
+      await tx.wait();
+      return true;
+    } catch (err) {
+      console.log("error is ---",err);
+      return false;
+    }
+  };
+
+  const getOwnerCases = async (address) => {
+    // Connect to a custom JSON-RPC endpoint
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    const signer = await provider.getSigner();
+    try {
+      const contract = new ethers.Contract(CONTRACT_ADDRESS, abi, signer);
+
+      const tx = await contract.getUsersCases(address);
+
+      alert("Fetched Values");
+      return tx;
+    } catch (err) {
+      alert("process failed");
+      console.log("error is ---",err);
+      return null;
+    }
+  };
+
+  const getAgreementDetails = async (agreementId) => {
+    // Connect to a custom JSON-RPC endpoint
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    const signer = await provider.getSigner();
+
+    try {
+      const contract = new ethers.Contract(CONTRACT_ADDRESS, abi, signer);
+
+      const tx = await contract.getAgreementDetails(agreementId);
+      console.log("vallues is ", tx);
+      return tx;
+    } catch (err) {
+      console.log("error is ---",err);
+      return null;
+    }
+  };
+
+  const getParty1Agreements = async (agreementId) => {
+    // Connect to a custom JSON-RPC endpoint
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    const signer = await provider.getSigner();
+
+    try {
+      const contract = new ethers.Contract(CONTRACT_ADDRESS, abi, signer);
+
+      const tx = await contract.getParty1Agreements(agreementId);
+      console.log("vallues is ", tx);
+      return tx;
+    } catch (err) {
+      console.log("error is ---",err);
+      return null;
+    }
+  };
+
+  const getParty2Agreements = async (agreementId) => {
+    // Connect to a custom JSON-RPC endpoint
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    const signer = await provider.getSigner();
+
+    try {
+      const contract = new ethers.Contract(CONTRACT_ADDRESS, abi, signer);
+
+      const tx = await contract.getParty2Agreements(agreementId);
+      console.log("vallues is ", tx);
+      return tx;
+    } catch (err) {
+      console.log("error is ---",err);
+      return null;
+    }
+  };
+
+
 
   const contextValue = {
     address,
@@ -56,107 +209,3 @@ BlockchainContextProvider.propTypes = {
   children: PropTypes.node.isRequired,
 };
 
-
-//   // A function that takes a solidity output as a string and returns a readable time as a string
-//   function convertToReadableTime(solidityOutput) {
-//     // Convert the BigNumber object to a number
-//     const seconds = Number(solidityOutput);
-
-//     // Create a Date object from the number
-//     const date = new Date(seconds * 1000); // multiply by 1000 to get milliseconds
-
-//     // Format the Date object as a readable string
-//     const readableTime = date.toLocaleString(); // use your preferred options
-
-//     // Return the readable time
-//     return readableTime;
-//   }
-
-//   const createCase = async (hash, address) => {
-//     try {
-//       // Connect to a custom JSON-RPC endpoint
-//       const provider = new ethers.BrowserProvider(window.ethereum);
-//       const signer = await provider.getSigner();
-
-//       const contract = new ethers.Contract(CONTRACT_ADDRESS, abi, signer);
-
-//       const tx = await contract.createCase(hash, address);
-//       await tx.wait();
-//       alert("Process Successful");
-//     } catch (err) {
-//       alert("process failed");
-//       alert(err);
-//     }
-//   };
-
-//   const addWitness = async (caseId, witnessAddress) => {
-//     try {
-//       // Connect to a custom JSON-RPC endpoint
-//       const provider = new ethers.BrowserProvider(window.ethereum);
-//       const signer = await provider.getSigner();
-
-//       const contract = new ethers.Contract(CONTRACT_ADDRESS, abi, signer);
-
-//       const tx = await contract.addWitness(caseId, witnessAddress);
-//       await tx.wait();
-//       alert("Process Successful");
-//     } catch (err) {
-//       alert("process failed");
-//       alert(err);
-//     }
-//   };
-
-//   const uploadEvidence = async (caseId, evidenceData) => {
-//     try {
-//       // Connect to a custom JSON-RPC endpoint
-//       const provider = new ethers.BrowserProvider(window.ethereum);
-//       const signer = await provider.getSigner();
-
-//       const contract = new ethers.Contract(CONTRACT_ADDRESS, abi, signer);
-
-//       const tx = await contract.uploadEvidence(caseId, evidenceData);
-//       await tx.wait();
-//       alert("Process Successful");
-//     } catch (err) {
-//       alert("process failed");
-//       alert(err);
-//     }
-//   };
-
-//   const getOwnerCases = async (address) => {
-//     // Connect to a custom JSON-RPC endpoint
-//     const provider = new ethers.BrowserProvider(window.ethereum);
-//     const signer = await provider.getSigner();
-//     try {
-//       const contract = new ethers.Contract(CONTRACT_ADDRESS, abi, signer);
-
-//       const tx = await contract.getUsersCases(address);
-
-//       alert("Fetched Values");
-//       return tx;
-//     } catch (err) {
-//       alert("process failed");
-//       alert(err);
-//       return null;
-//     }
-//   };
-
-//   const getCaseDetail = async (fileId) => {
-//     // Connect to a custom JSON-RPC endpoint
-//     const provider = new ethers.BrowserProvider(window.ethereum);
-//     const signer = await provider.getSigner();
-
-//     try {
-//       const contract = new ethers.Contract(CONTRACT_ADDRESS, abi, signer);
-
-//       const tx = await contract.getCaseDetails(fileId);
-
-//       alert("Fetched Values");
-//       console.log("vallues is ", tx);
-//       return tx;
-//     } catch (err) {
-//       alert("process failed");
-//       alert(err);
-//       return null;
-//     }
-//   };
